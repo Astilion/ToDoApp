2ece3aebb917b4bb674972757a93372f517ed644
Mateusz <mkaminski.contact@gmail.com>
2023-11-20 10:01:31 +0100

    Changed NewTask.tsx select options into radio elements

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -4,13 +4,18 @@ const NewTask = () => {
 			<input type='text' placeholder='Add task...' />
 			<button>ADD</button>
 			<label htmlFor='tasks'>Category:</label>
-			<select name='tasks' id='tasks'>
-				<option value='home'>Home</option>
-				<option value='sport'>Sport</option>
-				<option value='relax'>Relax</option>
-				<option value='finances'>Finances</option>
-				<option value='shopping'>Shopping</option>
-			</select>
+			<div>
+				<input type='radio' id='home' name='tasks' />
+				<label htmlFor='home'>Home</label>
+				<input type='radio' id='sport' name='tasks' />
+				<label htmlFor='sport'>Sport</label>
+				<input type='radio' id='shopping' name='tasks' />
+				<label htmlFor='shopping'>Shopping</label>
+				<input type='radio' id='finances' name='tasks' />
+				<label htmlFor='finances'>Finances</label>
+				<input type='radio' id='work' name='tasks' />
+				<label htmlFor='work'>Work</label>
+			</div>
 		</div>
 	);
 };
