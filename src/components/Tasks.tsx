3acd450b9ed4d2a9af92af3606fd2ab46d6cc12b
Mateusz <mkaminski.contact@gmail.com>
2023-11-23 14:41:30 +0100

    change state to constant

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,14 +1,16 @@
 import Task from "./Task";
-
 interface TasksProps {
 	tasks: { id: string; category: string; name: string }[];
 }
 const Tasks = ({ tasks }: TasksProps) => {
+	const isEmpty = tasks.length === 0;
 	return (
 		<div className='bg-slate-300 p-4'>
 			<div className='text-center mb-3'>
 				<h3 className='font-bold text-lg text-center mb-1'>List of tasks</h3>
-				<p className='text-red-500 font-semibold'>No tasks on the list</p>
+				{isEmpty && (
+					<p className='text-red-500 font-semibold'>No tasks on the list</p>
+				)}
 			</div>
 			<ul>
 				{tasks.map(task => (
