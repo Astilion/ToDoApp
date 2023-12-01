07e89f63606d795885692c7ba1bbdde37bfc47a3
Mateusz <mkaminski.contact@gmail.com>
2023-12-01 18:07:44 +0100

    added dummy SideNav component

---
+++ src/components/ui/SideNav.tsx
@@ -0,0 +1,16 @@
+const SideNav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Dark/Light mode button</li>
+                <li>Login</li>
+                <li>Sign in</li>
+
+                <li>Calendar</li>
+                <li>Today</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default SideNav;
