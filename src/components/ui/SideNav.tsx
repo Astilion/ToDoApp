d3d7ee01ef4d3e6e39db9517c8c6c849141f5e56
Mateusz <mkaminski.contact@gmail.com>
2023-12-04 16:52:52 +0100

    placed checkbox inside button element

--- src/components/ui/SideNav.tsx
+++ src/components/ui/SideNav.tsx
@@ -8,6 +8,7 @@ const SideNav = () => {
 				</div>
 
 				<div>
+                    <button>
 					<label className='relative inline-flex items-center me-5 cursor-pointer'>
 						<input
 							type='checkbox'
@@ -18,6 +19,7 @@ const SideNav = () => {
 						<div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-yellow-300 dark:peer-focus:ring-yellow-800 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-yellow-400"></div>
 						<span className='ms-3 text-sm font-medium '></span>
 					</label>
+                    </button>
 				</div>
 			</div>
 			<ul className=''>
