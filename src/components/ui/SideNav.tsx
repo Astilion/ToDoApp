e75405213d51128bb75fa947ab3ab49c5a7597e6
Mateusz <mkaminski.contact@gmail.com>
2023-12-06 06:18:08 +0100

    changed toggle button color

--- src/components/ui/SideNav.tsx
+++ src/components/ui/SideNav.tsx
@@ -19,7 +19,7 @@ const SideNav = ({ handleThemeSwitch }: SideNavProps) => {
                 className="peer sr-only"
                 defaultChecked
               />
-              <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px]   after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-yellow-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700"></div>
+              <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px]   after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-gray-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700"></div>
               <span className="ms-3 text-sm font-medium "></span>
             </label>
           </button>
