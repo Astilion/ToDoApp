cd5e91d57a7c478cb17c96a862fe7586ccecf600
Mateusz <mkaminski.contact@gmail.com>
2023-12-07 06:02:43 +0100

    added Closing nav onClick + styles updating

--- src/components/ui/Navigation/SideNav.tsx
+++ src/components/ui/Navigation/SideNav.tsx
@@ -8,14 +8,21 @@ interface SideNavProps {
   handleThemeSwitch: () => void;
 }
 const SideNav = ({ handleThemeSwitch }: SideNavProps) => {
+  const initialNavClasses = 'fixed z-20 min-h-full w-full left-0 bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col md:left-0 md:relative';
   const [isNavOpen, setIsNavOpen] = useState(false);
-
+  const [navClasses, setNavClasses] = useState(initialNavClasses);
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
+
+    const updatedNavClasses = isNavOpen
+      ? 'fixed z-20 min-h-full w-full left-0 bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col md:left-0 md:relative'
+      : 'fixed z-20 min-h-full w-full -left-full bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col md:relative';
+    setNavClasses(updatedNavClasses);
+
     console.log(isNavOpen);
   };
   return (
-    <nav className=" fixed z-20 min-h-full w-full bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col">
+    <nav className={navClasses}>
       <div className="flex justify-between md:py-3 md:pl-4">
         <UserProfile />
         <div className="">
