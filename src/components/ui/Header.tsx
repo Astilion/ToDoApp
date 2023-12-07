88c7cf33d9c3df1f95389b7ff8b62ef5f73caf8f
Mateusz <mkaminski.contact@gmail.com>
2023-12-07 11:35:22 +0100

    added nav sliding effect

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="bg-sky-600 p-2 text-center md:rounded-t-xl">
+    <header className="bg-sky-700 p-2 text-center md:rounded-t-xl">
       <h1 className="text-shadow text-4xl font-bold text-white drop-shadow-2xl">
         ToDo List
       </h1>
--- src/components/ui/Navigation/SideNav.tsx
+++ src/components/ui/Navigation/SideNav.tsx
@@ -8,38 +8,31 @@ interface SideNavProps {
   handleThemeSwitch: () => void;
 }
 const SideNav = ({ handleThemeSwitch }: SideNavProps) => {
-  const initialNavClasses =
-    "fixed z-20 min-h-full w-full left-0 bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col md:left-0 md:relative";
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [navClasses, setNavClasses] = useState(initialNavClasses);
+
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
-
-    const updatedNavClasses = isNavOpen
-      ? "fixed z-20 min-h-full w-full left-0 bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col md:left-0 md:relative"
-      : "fixed z-20 min-h-full w-full -left-full bg-slate-100 dark:bg-gray-700 dark:text-white md:flex md:w-auto md:flex-col md:relative";
-    setNavClasses(updatedNavClasses);
-
     console.log(isNavOpen);
   };
   return (
     <>
-      {isNavOpen && (
-        <button className="fixed z-20 m-4" onClick={toggleNav}>
-          <IconLayoutSidebar color={window.innerWidth <= 768 ? "white" : "gray"} />
-        </button>
-      )}
+      <button className="fixed z-20 m-4" onClick={toggleNav}>
+        <IconLayoutSidebar color="gray" />
+      </button>
 
-      <nav className={navClasses}>
+      <nav
+        className={`fixed left-0 z-20 min-h-full w-full bg-slate-100 transition-transform dark:bg-gray-700 dark:text-white md:relative md:left-0 md:flex md:w-auto md:flex-col ${
+          isNavOpen ? "transform-none" : "-translate-x-full"
+        }`}
+      >
         <div className="flex justify-between py-3 md:pl-4">
           <UserProfile />
           <div className="">
             <ThemeSwitch handleThemeSwitch={handleThemeSwitch} />
-            {!isNavOpen && (
-              <button className="mr-4" onClick={toggleNav}>
-                <IconLayoutSidebar color="gray" />
-              </button>
-            )}
+
+            <button className="mr-4" onClick={toggleNav}>
+              <IconLayoutSidebar color="gray" />
+            </button>
           </div>
         </div>
         <div className="mb-2 h-px w-full bg-slate-300 dark:bg-gray-600"></div>
