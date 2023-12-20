0c9e8d59f7edd5282f47c565dc87b6780e5173be
Mateusz <mkaminski.contact@gmail.com>
2023-12-20 11:10:23 +0100

    started styling of login/signin popup

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
-
+import { useState } from "react";
 
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
@@ -10,20 +8,28 @@ function AuthForm() {
   }
 
   return (
-    <div className='flex  justify-center  items-center bg-slate-200 dark:bg-gray-600'>
-      <form method="post">
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
-        <p>
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
-        </p>
-        <p>
-          <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
-        </p>
+    <div className="flex items-center justify-center bg-slate-200 dark:bg-gray-600">
+      <form method="post" className="flex flex-col">
+        <h2 className="py-4 text-3xl font-semibold">
+          {isLogin ? "Log in" : "Create a new user"}
+        </h2>
+        <input
+          className="p-1/2 mb-1 h-auto text-xl"
+          type="text"
+          name="username"
+          placeholder="Email Address"
+          required
+        />
+        <input
+          type="password"
+          className="p-1/2 mb-1 h-auto text-xl"
+          name="password"
+          placeholder="Password"
+          required
+        />
         <div>
           <button onClick={switchAuthHandler} type="button">
-            {isLogin ? 'Create new user' : 'Login'}
+            {isLogin ? "Create new user" : "Login"}
           </button>
           <button>Save</button>
         </div>
