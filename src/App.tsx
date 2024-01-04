bc0e51115844065adf61f2cb086d128ea588423c
Mateusz <mkaminski.contact@gmail.com>
2024-01-04 10:29:42 +0100

    fixed typescript error in fetchTasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,20 @@
 import { useTheme } from "./utils/themeUtils.ts";
+import { useTaskList } from "./utils/taskUtils.ts";
 import Header from "./components/ui/Header";
 import NewTask from "./components/NewTask";
 import Tasks from "./components/Tasks";
 import SideNav from "./components/ui/Navigation/SideNav";
 import AuthForm from "./components/auth/AuthForm";
-import { useEffect, useState } from "react";
-import { db } from "./config/firebase.js";
-import { getDocs, collection, addDoc } from "firebase/firestore";
+import { useState } from "react";
 import Modal from "./components/Modal";
 
-interface tasks {
-  id: string;
-  TaskName: string;
-}
 function App() {
-  const {handleThemeSwitch} = useTheme()
-  const [tasks2, setTasks2] = useState<tasks[]>([]);
-  const tasksCollectionRef = collection(db, "tasks");
+  const { handleThemeSwitch } = useTheme();
+  const { tasks, addTaskHandler } = useTaskList();
   const [isModalOpen, setIsModalOpen] = useState(true);
 
   const closeModal = () => {
     setIsModalOpen(false);
-  };
-
-  const getTaskList = async () => {
-    try {
-      const data = await getDocs(tasksCollectionRef);
-      const filteredData = data.docs.map((doc) => ({
-        ...doc.data(),
-        id: doc.id,
-      }));
-      setTasks2(filteredData);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  useEffect(() => {
-    getTaskList();
-  }, []);
-
-  const addTaskHandler = async (inputValue: string) => {
-    try {
-      const newTask = {
-        TaskName: inputValue,
-      };
-
-      const docRef = await addDoc(tasksCollectionRef, newTask);
-      setTasks2((prevTasks) => [...prevTasks, { ...newTask, id: docRef.id }]);
-    } catch (err) {
-      console.error("Error adding task: ", err);
-    }
   };
 
   return (
@@ -65,7 +30,7 @@ function App() {
         <div className="md:min-w-3xl relative w-full  md:mx-auto md:mt-10 md:max-w-3xl">
           <Header />
           <NewTask addTaskHandler={addTaskHandler} />
-          <Tasks tasks={tasks2} />
+          <Tasks tasks={tasks} />
         </div>
       </div>
     </>
---
+++ src/utils/taskUtils.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { collection, getDocs, addDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+interface Task {
+  id: string;
+  TaskName: string;
+}
+export const useTaskList = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const tasksCollectionRef = collection(db, "tasks");
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const data = await getDocs(tasksCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as Task[];
+        setTasks(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchTasks();
+  }, [tasksCollectionRef]);
+
+  const addTaskHandler = async (inputValue: string) => {
+    try {
+      const newTask = {
+        TaskName: inputValue,
+      };
+      const docRef = await addDoc(tasksCollectionRef, newTask);
+      setTasks((prevTasks) => [...prevTasks, { ...newTask, id: docRef.id }]);
+    } catch (err) {
+      console.log("Error adding task: ", err);
+    }
+  };
+
+  return { tasks, addTaskHandler };
+};
